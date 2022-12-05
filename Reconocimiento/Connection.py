67f8fefeb1688b5f2373f4a065ec692d45c7f027
BoyPhoenix <93331056+BoyPhoenix@users.noreply.github.com>
2022-12-05 16:23:13 -0500

    Change to pers_doc and fix errors

--- Reconocimiento/Connection.py
+++ Reconocimiento/Connection.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 @app.route("/compare", methods=["POST"])
 def face():
     imagefile = request.files["image"]
-    pers_id = request.form["pers_id"]
+    pers_doc = request.form["pers_doc"]
     filename =  utils.secure_filename(imagefile.filename)
     namefile = "./" + filename
     imagefile.save(namefile)
     _,cursor = Query.get_connection_cursor_tuple()
-    b64 = Query.select_photo(cursor,pers_id)
+    b64 = Query.select_photo(cursor,pers_doc)
     Result = Recognition.CompareFF(namefile,b64)
     os.remove(namefile)
     os.remove("./image.jpg")
@@ -24,13 +24,12 @@ def face():
 @app.route("/update", methods=["POST"])
 def update():
     imagefile = request.files["image"]
-    pers_id = request.form["pers_id"]
+    pers_doc = request.form["pers_doc"]
     filename =  utils.secure_filename(imagefile.filename)
     namefile = "./" + filename
     imagefile.save(namefile)
     connection ,cursor = Query.get_connection_cursor_tuple()
-    Result = Query.update_photo(connection,cursor,namefile,pers_id)
-    print(Result)
+    Query.update_photo(connection,cursor,namefile,pers_doc)
     os.remove(namefile)
     return jsonify({
         #Image has been changed
